af50117d772f384aced022d01f710d1f8ad96322
AloneDevelopment <Alonedevelopments@gmail.com>
2022-03-27 15:33:14 +0800

    delete

--- src/main/java/com/starmediadev/plugins/starspawners/StarSpawners.java
+++ src/main/java/com/starmediadev/plugins/starspawners/StarSpawners.java
@@ -1,29 +1,16 @@
 package com.starmediadev.plugins.starspawners;
 
-import com.starmediadev.plugins.starspawners.commands.CommandTab;
-import com.starmediadev.plugins.starspawners.commands.SpawnerGive;
-import com.starmediadev.plugins.starspawners.events.SpawnerBreak;
-import com.starmediadev.plugins.starspawners.events.SpawnerPlace;
-import com.starmediadev.plugins.starspawners.utils.Util;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class StarSpawners extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-        new SpawnerGive(this);
-        new CommandTab();
-        new SpawnerBreak(this);
-        new SpawnerPlace(this);
-        saveDefaultConfig();
-
-        Util.consoleMsg("=============================== \n Spawners have started! \n ===============================");
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
