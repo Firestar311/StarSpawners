80ae9a868c8274e0e654c300e317f3ba06f53b20
Firestar311 <slaterandrew0398@gmail.com>
2022-03-30 14:38:50 -0500

    Changed detection of the command sender and using the pattern-matching feature

--- src/main/java/com/starmediadev/plugins/starspawners/commands/SpawnerCommand.java
+++ src/main/java/com/starmediadev/plugins/starspawners/commands/SpawnerCommand.java
@@ -1,5 +1,6 @@
 package com.starmediadev.plugins.starspawners.commands;
 
+import com.starmediadev.plugins.starmcutils.util.MCUtils;
 import com.starmediadev.plugins.starspawners.StarSpawners;
 import com.starmediadev.plugins.starspawners.managers.GiveSpawner;
 import org.bukkit.Bukkit;
@@ -20,73 +21,86 @@ import java.util.Collections;
 import java.util.List;
 
 public class SpawnerCommand implements TabExecutor {
-
+    
     private StarSpawners main;
-
+    
     public SpawnerCommand(StarSpawners main) {
         this.main = main;
     }
-
+    
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-            if(args[0].equalsIgnoreCase("give")) {
-                if (!player.hasPermission("StarSpawners.admin")) return false;
-                if (args.length != 4) return false;
-                Player p = Bukkit.getPlayer(args[1]);
-                String spawner = args[2];
-                int amount = Integer.parseInt(args[3]);
-                new GiveSpawner(main, player, amount, spawner.toUpperCase());
-                return true;
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage(MCUtils.color("&cOnly players can use that command"));
+            return true;
+        }
+        if (args[0].equalsIgnoreCase("give")) {
+            if (!player.hasPermission("StarSpawners.admin")) {
+                return false;
             }
-            if (args[0].equalsIgnoreCase("set")) {
-                if (args.length != 2) return false;
-                if (!player.hasPermission("StarSpawners.admin")) return false;
-                Block block = player.getTargetBlock(null, 4);
-                if (!block.getType().equals(Material.SPAWNER)) return false;
-                CreatureSpawner creatureSpawner = (CreatureSpawner) block.getState();
-                creatureSpawner.setSpawnedType(EntityType.valueOf(args[1].toUpperCase()));
-
-                new BukkitRunnable() {
-
-                    @Override
-                    public void run() {
-                        creatureSpawner.update();
-                    }
-                }.runTaskLater(main, 1);
-                return true;
+            if (args.length != 4) {
+                return false;
             }
+            Player p = Bukkit.getPlayer(args[1]);
+            String spawner = args[2];
+            int amount = Integer.parseInt(args[3]);
+            new GiveSpawner(main, player, amount, spawner.toUpperCase());
+            return true;
+        }
+        if (args[0].equalsIgnoreCase("set")) {
+            if (args.length != 2) {
+                return false;
+            }
+            if (!player.hasPermission("StarSpawners.admin")) {
+                return false;
+            }
+            Block block = player.getTargetBlock(null, 4);
+            if (!block.getType().equals(Material.SPAWNER)) {
+                return false;
+            }
+            CreatureSpawner creatureSpawner = (CreatureSpawner) block.getState();
+            creatureSpawner.setSpawnedType(EntityType.valueOf(args[1].toUpperCase()));
+            
+            new BukkitRunnable() {
+                
+                @Override
+                public void run() {
+                    creatureSpawner.update();
+                }
+            }.runTaskLater(main, 1);
+            return true;
         }
         return false;
     }
-
+    
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
         List<String> completions = new ArrayList<>();
         List<String> cmdList = new ArrayList<>();
-        if(!sender.hasPermission("StarSpawner.admin")) return completions;
-        if(args.length == 1) {
+        if (!sender.hasPermission("StarSpawner.admin")) {
+            return completions;
+        }
+        if (args.length == 1) {
             cmdList.add("give");
             cmdList.add("set");
             StringUtil.copyPartialMatches(args[0], cmdList, completions);
         }
-        if(args.length == 2) {
-            if(args[0].equalsIgnoreCase("give")) {
+        if (args.length == 2) {
+            if (args[0].equalsIgnoreCase("give")) {
                 cmdList.add("<player>");
             } else {
                 cmdList.add("<entity>");
             }
             StringUtil.copyPartialMatches(args[1], cmdList, completions);
         }
-        if(args.length == 3) {
-            if(args[0].equalsIgnoreCase("give")) {
+        if (args.length == 3) {
+            if (args[0].equalsIgnoreCase("give")) {
                 cmdList.add("<entity>");
                 StringUtil.copyPartialMatches(args[2], cmdList, completions);
             }
         }
-        if(args.length == 4) {
-            if(args[0].equalsIgnoreCase("give")) {
+        if (args.length == 4) {
+            if (args[0].equalsIgnoreCase("give")) {
                 cmdList.add("<amount>");
                 StringUtil.copyPartialMatches(args[3], cmdList, completions);
             }
